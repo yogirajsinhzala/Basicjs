9e7e2d290976e790bb95cf2045261b946d407e0d
Yogiraj Zala <yogirajzala31@gmail.com>
2018-06-07 18:36:59 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req,res){
+    
+    res.send("Hey there, Welcome to my assignment!");
+         
+   });
+   
+
+app.get("/speak/:animal", function(req, res){
+    
+        
+    var sounds = {
+            pig: "Oink",
+            Giraffes: "bleat",
+            Bats: "screech",
+            Crows: "caw",
+            Ferrets: "dook",
+            
+    }
+    
+    var animal = req.params.animal.toLowerCase();
+    
+    var sound = sounds[animal];
+    
+
+    res.send("The "+ animal + " Says '"+sound+"'. ");
+   
+});   
+   
+   
+app.get("/repeat/:message/:times", function(req, res){
+    
+    var message = req.params.message;
+    var times = Number(req.params.times);
+    var result = "";
+    for(var i = 0; i< times; i++){
+        
+        result+= message + " ";
+    }    
+    
+    res.send(result);
+    
+});   
+
+app.get("*", function(req, res) {
+    
+    res.send("Page not found. ");
+     
+});
+   
+app.listen(process.env.PORT, process.env.IP, function(){
+    
+    console.log("Your server has started!")
+});   
